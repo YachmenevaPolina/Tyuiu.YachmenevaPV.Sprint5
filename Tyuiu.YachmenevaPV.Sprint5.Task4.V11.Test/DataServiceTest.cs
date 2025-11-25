8adacce5c9d30b75de92b86b18d030aec4954fb5
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-11-25 14:45:47 +0500

    Task4

---
+++ Tyuiu.YachmenevaPV.Sprint5.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.YachmenevaPV.Sprint5.Task4.V11.Lib;
+namespace Tyuiu.YachmenevaPV.Sprint5.Task4.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask4V11.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+
+            Assert.AreEqual(wait, fileExist);
+        }
+    }
+}
---
+++ Tyuiu.YachmenevaPV.Sprint5.Task4.V11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using Tyuiu.YachmenevaPV.Sprint5.Task4.V11.Lib;
+{
+    DataService ds = new DataService();
+    Console.Title = "Спринт #5 | Выполнила: Ячменёва П. В. | РППб-25-1";
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* Спринт #5                                                               *");
+    Console.WriteLine("* Тема: Чтение данныхиз текстового файла.                                 *");
+    Console.WriteLine("* Задание #4                                                              *");
+    Console.WriteLine("* Вариант #11                                                             *");
+    Console.WriteLine("* Выполнила: Ячменёва Полина Викторовна | РППб-25-1                       *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* УСЛОВИЕ:                                                                *");
+    Console.WriteLine("* Вычислить значение по формуле.                                          *");
+    Console.WriteLine("*                                                                         *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+    Console.WriteLine("***************************************************************************");
+
+    string path = @"C:\DataSprint5\InPutDataFileTask4V11.txt";
+    Console.WriteLine("Данные находятся в файле " + path);
+
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+    Console.WriteLine("***************************************************************************");
+
+    double res = ds.LoadFromDataFile(path);
+    Console.WriteLine(res);
+    Console.ReadKey();
+}
