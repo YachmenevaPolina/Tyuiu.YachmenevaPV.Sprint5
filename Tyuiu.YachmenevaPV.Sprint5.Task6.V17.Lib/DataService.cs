66f12255f53da0ec8cbc56cbe9523766f61903d3
Tyuiu.YachmenevaPV@outlook.com <poliviya7@gmail.com>
2025-11-27 20:40:44 +0500

    Task6

---
+++ Tyuiu.YachmenevaPV.Sprint5.Task6.V17.Lib/DataService.cs
@@ -0,0 +1,36 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.YachmenevaPV.Sprint5.Task6.V17.Lib
+{
+    public class DataService : ISprint5Task6V17
+    {
+        public int LoadFromDataFile(string path)
+        {
+            string line;
+
+            using (StreamReader reader = new StreamReader(path)) { line = reader.ReadLine(); }
+            
+            int count = 0;
+            int run = 0;
+
+            foreach (char ch in line)
+            {
+                if (ch == ' ')
+                {
+                    run++;
+                }
+                else
+                {
+                    if (run > 1)
+                        count++;
+
+                    run = 0;
+                }
+            }
+
+            if (run > 1)
+                count++;
+
+            return count;
+        }
+    }
+}
---
+++ Tyuiu.YachmenevaPV.Sprint5.Task6.V17/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.YachmenevaPV.Sprint5.Task6.V17.Lib;
+{
+    DataService ds = new DataService();
+    Console.Title = "Спринт #5 | Выполнила: Ячменёва П. В. | РППб-25-1";
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* Спринт #5                                                               *");
+    Console.WriteLine("* Тема: Обработка текстовых данных.                                       *");
+    Console.WriteLine("* Задание #6                                                              *");
+    Console.WriteLine("* Вариант #17                                                             *");
+    Console.WriteLine("* Выполнила: Ячменёва Полина Викторовна | РППб-25-1                       *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* УСЛОВИЕ:                                                                *");
+    Console.WriteLine("* Найти количество лишних пробелов в заданной строке.                     *");
+    Console.WriteLine("*                                                                         *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+    Console.WriteLine("***************************************************************************");
+
+    string directory = Path.Combine("C:", "DataSprint5");
+    string path = Path.Combine(directory, "InPutDataFileTask6V17.txt");
+
+    Console.WriteLine("Данные находятся в файле:" +  path);
+
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+    Console.WriteLine("***************************************************************************");
+
+    int res = ds.LoadFromDataFile(path);
+    Console.WriteLine(res);
+    Console.ReadKey();
+}
